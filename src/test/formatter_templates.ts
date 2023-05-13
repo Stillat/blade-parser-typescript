880fb41d649d4661886980f88d7f4de720719720
John Koster <john@stillat.com>
2023-05-13 10:03:54 -0500

    Nested loop coverage

--- src/test/formatter_templates.ts
+++ src/test/formatter_templates.ts
@@ -178,4 +178,51 @@ suite('General Template Formatting', () => {
 `;
         assert.strictEqual(formatBladeString(template), out);
     });
+
+    test('it indents nested loops', () => {
+        const template = `@extends('layouts.master')
+
+@section('title', 'Product Categories')
+
+@section('content')
+<h1>Product Categories</h1>
+
+@foreach ($categories as $category)
+<h2>{{ $category->name }}</h2>
+
+@if ($category->products->isEmpty())
+<p>No products in this category.</p>
+@else
+<ul>
+@foreach ($category->products as $product)
+<li>{{ $product->name }} - \${{ $product->price }}</li>
+@endforeach
+</ul>
+@endif
+@endforeach
+@endsection`;
+        const out = `@extends("layouts.master")
+
+@section('title', 'Product Categories')
+
+@section("content")
+    <h1>Product Categories</h1>
+
+    @foreach($categories as $category)
+        <h2>{{ $category->name }}</h2>
+
+        @if($category->products->isEmpty())
+            <p>No products in this category.</p>
+        @else
+            <ul>
+                @foreach($category->products as $product)
+                    <li>{{ $product->name }} - \${{ $product->price }}</li>
+                @endforeach
+            </ul>
+        @endif
+    @endforeach
+@endsection
+`;
+        assert.strictEqual(formatBladeString(template), out);
+    });
 });
