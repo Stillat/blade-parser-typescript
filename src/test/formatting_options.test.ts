2e56e6cfe81ed991df9b1ec521581e24b6184186
John Koster <john@stillat.com>
2023-05-14 11:21:55 -0500

    More coverage #7 #10 #9 #5

--- src/test/formatting_options.test.ts
+++ src/test/formatting_options.test.ts
@@ -40,4 +40,110 @@ suite('Formatting Options', () => {
             formatDirectivePhpParameters: false,
         }).trim(), template);
     });
+
+    test('it continues to format the document even with certain things disabled', () => {
+        // Note: the spacing on 'foo4' is intentional.
+        const template = `@extends('layouts.master')
+
+@props([
+    'foo' => [],
+    'foo1' => [],
+    'foo2' => [],
+    'foo3' => [],
+    'foo4'      => [],
+    'foo5' => [],
+    'foo6' => [],
+    'foo7' => [
+        'foo' => [],
+        'foo1' => [],
+        'foo2' => [],
+        'foo3' => [],
+        'foo4' => [],
+        'foo5' => [],
+        'foo6' => [],
+        'foo7' => [],
+    ],
+])
+
+@section('title', 'Dashboard')
+
+<div @class([
+    'foo' => true
+])>
+    Something
+</div>
+
+@section('content')
+<h1>Dashboard</h1>
+
+@if(Auth::check())
+<p>Welcome back, {{ Auth::user()->name }}!</p>
+
+@if(Auth::user()->isAdmin())
+<p>You have <a href="">admin</a> privileges.</p>
+@else
+<p>You are a regular user.</p>
+@endif
+@else
+<p>Please <a href="{{ route('login') }}">log in</a> to continue.</p>
+@endif
+@endsection`;
+        const out = `@extends('layouts.master')
+
+@props([
+    'foo' => [],
+    'foo1' => [],
+    'foo2' => [],
+    'foo3' => [],
+    'foo4'      => [],
+    'foo5' => [],
+    'foo6' => [],
+    'foo7' => [
+        'foo' => [],
+        'foo1' => [],
+        'foo2' => [],
+        'foo3' => [],
+        'foo4' => [],
+        'foo5' => [],
+        'foo6' => [],
+        'foo7' => [],
+    ],
+])
+
+@section('title', 'Dashboard')
+
+<div @class([
+    'foo' => true
+])>Something</div>
+
+@section('content')
+    <h1>Dashboard</h1>
+
+    @if (Auth::check())
+        <p>Welcome back, {{ Auth::user()->name }}!</p>
+
+        @if (Auth::user()->isAdmin())
+            <p>
+                You have
+                <a href="">admin</a>
+                privileges.
+            </p>
+        @else
+            <p>You are a regular user.</p>
+        @endif
+    @else
+        <p>
+            Please
+            <a href="{{ route('login') }}">log in</a>
+            to continue.
+        </p>
+    @endif
+@endsection
+`;
+        assert.strictEqual(formatBladeString(template, {
+            ...defaultSettings,
+            formatInsideEcho: false,
+            formatDirectivePhpParameters: false,
+        }), out);
+    });
 });
