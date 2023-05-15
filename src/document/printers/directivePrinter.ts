b7a66e399eb4e6a7bb964062e4228553f0d2d0bc
Zep Fietje <hey@zepfietje.com>
2023-05-15 08:54:11 +0200

    Add missing control directives

--- src/document/printers/directivePrinter.ts
+++ src/document/printers/directivePrinter.ts
@@ -6,7 +6,7 @@ import { TransformOptions } from '../transformOptions';
 
 export class DirectivePrinter {
     private static defaultControlDirectiveNames:string[] = [
-        'if', 'elseif', 'unless', 'foreach', 'for'
+        'if', 'elseif', 'unless', 'while', 'for', 'foreach', 'forelse'
     ];
 
     static printDirective(directive: DirectiveNode, options: TransformOptions, phpFormatter: PhpFormatter | null, jsonFormatter: JsonFormatter | null): string {
