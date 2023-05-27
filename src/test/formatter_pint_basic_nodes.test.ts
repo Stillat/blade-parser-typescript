63229be5889f1665024308cb51d329641bc79251
John Koster <john@stillat.com>
2023-05-27 15:34:44 -0500

    More coverage

--- src/test/formatter_pint_basic_nodes.test.ts
+++ src/test/formatter_pint_basic_nodes.test.ts
@@ -1,5 +1,71 @@
 import assert from 'assert';
+import { formatBladeStringWithPint } from '../formatting/prettier/utils';
 
 suite('Pint Transformer: Basic Nodes', () => {
+    test('pint: it formats echos', () => {
+        assert.strictEqual(
+            formatBladeStringWithPint('{{ $title }}').trim(),
+            "{{ $title }}"
+        );
+    });
 
+    test('pint: it formats echo variant', () => {
+        assert.strictEqual(
+            formatBladeStringWithPint('{{{ $title }}}').trim(),
+            "{{{ $title }}}"
+        );
+    });
+
+    test('pint: it formats escaped echos', () => {
+        assert.strictEqual(
+            formatBladeStringWithPint('{!! $title !!}').trim(),
+            "{!! $title !!}"
+        );
+    });
+
+    test('pint: it formats simple comments', () => {
+        assert.strictEqual(
+            formatBladeStringWithPint('{{-- Comment. --}}').trim(),
+            "{{-- Comment. --}}"
+        );
+    });
+
+    test('pint: it formats block comments', () => {
+        assert.strictEqual(
+            formatBladeStringWithPint(`{{--
+                Block Comment. --}}`).trim(),
+            `{{--
+    Block Comment.
+--}}`
+        );
+    });
+
+    test('pint: it formats block comments with', () => {
+        assert.strictEqual(
+            formatBladeStringWithPint(`{{--
+ Block Comment.
+ with many lines and with <stuff></stuff> --}}`),
+            `{{--
+    Block Comment.
+    with many lines and with <stuff></stuff>
+--}}
+`
+        );
+    });
+
+    test('pint: block comments inside a div', () => {
+        assert.strictEqual(
+            formatBladeStringWithPint(`<div>
+        {{--
+ Block Comment.
+ with many lines and with <stuff></stuff> --}}
+        </div>`).trim(),
+            `<div>
+    {{--
+        Block Comment.
+        with many lines and with <stuff></stuff>
+    --}}
+</div>`
+        );
+    });
 });
--- src/test/formatter_pint_can.test.ts
+++ src/test/formatter_pint_can.test.ts
@@ -1,5 +1,30 @@
 import assert from 'assert';
+import { formatBladeStringWithPint } from '../formatting/prettier/utils';
 
 suite('Pint Transformer: Can Nodes', () => {
+    test('pint: it can format can directive without errors', () => {
+        const template = `{{-- foo.blade.php --}}
 
+@props([
+"model",
+])
+
+@can('update', $model)
+update
+@else
+any
+@endcan`;
+        const expected = `{{-- foo.blade.php --}}
+
+@props([
+    'model',
+])
+
+@can('update', $model)
+    update
+@else
+    any
+@endcan`;
+        assert.strictEqual(formatBladeStringWithPint(template).trim(), expected.trim());
+    });
 });
