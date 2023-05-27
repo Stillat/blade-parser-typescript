5cdc6e4643d77dcb03598795eda4170c04aa9f3b
John Koster <john@stillat.com>
2023-05-27 18:43:39 -0500

    Delete temp.php

--- temp.php
+++
@@ -1,9 +0,0 @@
-0=DIR================================
-<?php $tVar = pintFn(true
-); ?>
-1=DIR================================
-<?php $tVar = pintFn(false
-); ?>
-2=DIR================================
-<?php $tVar = pintFn('something'
-); ?>
