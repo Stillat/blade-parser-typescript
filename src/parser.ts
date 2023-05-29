2149f5632e115668b43b9293d15d2907e544d913
John Koster <john@stillat.com>
2023-05-29 11:05:16 -0500

    Update parser.ts

--- src/parser.ts
+++ src/parser.ts
@@ -1,21 +1,3 @@
-import { BladeDocument } from './document/bladeDocument';
-import { PintTransformer } from './document/pintTransformer';
-import * as fs from 'fs';
-import { StringUtilities } from './utilities/stringUtilities';
-import { formatBladeString, formatBladeStringWithPint } from './formatting/prettier/utils';
-const { execSync } = require('child_process');
-
 // Internal file used to debug the parser.
-const input = `@props([
-    'actions' => [],
-    'url',
-])`;
 
-
-console.log(formatBladeStringWithPint(input));
-
-//console.log(theRes, resultMapping);
 debugger;
-
-
-
