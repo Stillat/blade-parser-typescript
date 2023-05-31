661d3362eca4ee4d299bbe4045d47d648ca7fb76
John Koster <john@stillat.com>
2023-05-31 12:13:50 -0500

    Update parser.ts

--- src/parser.ts
+++ src/parser.ts
@@ -1,12 +1,3 @@
 // Internal file used to debug the parser.
 
-import { setPrettierFilePath } from './formatting/optionDiscovery';
-import { formatBladeString, formatBladeStringWithPint } from './formatting/prettier/utils';
-import { formatBladeFilesInDirecetory } from './utilities/validationUtilities';
-
-const template = `<?php
-    fn () => true;
-?>`;
-console.log(formatBladeStringWithPint(template));
-
 debugger;
