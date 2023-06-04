9b8c32f7bc02bbfb5ef9832ca924b1fe67c5959a
John Koster <john@stillat.com>
2023-06-04 08:17:16 -0500

    Start of rule engine/config

---
+++ src/formatting/classStringsConfig.ts
@@ -0,0 +1,175 @@
+import { BladeDocument } from '../document/bladeDocument';
+import { BladeEchoNode, DirectiveNode, InlinePhpNode } from '../nodes/nodes';
+
+export interface IClassRuleset {
+    includeWhen: string[],
+    excludeWhen: string[]
+}
+
+export interface IClassStringConfiguration {
+    enabled: boolean,
+
+    directivesEnabled: boolean,
+    excludedDirectives: string[],
+    directives: IClassRuleset[],
+
+    bladePhpEnabled: boolean,
+    phpTagsEnabled: boolean,
+    phpTagRules: IClassRuleset[],
+
+    bladeEchoEnabled: boolean,
+    bladeEchoRules: IClassRuleset[],
+
+    documentRules: IClassRuleset[],
+    stringRules: IClassRuleset[],
+}
+
+export class ClassStringRuleEngine {
+    private config: IClassStringConfiguration;
+
+    private directiveExcludeRules: string[] = [];
+    private directiveIncludeRules: string[] = [];
+
+    private phpTagExcludeRules: string[] = [];
+    private phpTagIncludeRules: string[] = [];
+
+    private echoExcludeRules: string[] = [];
+    private echoIncludeRules: string[] = [];
+
+    private documentExcludeRules: string[] = [];
+    private documentIncludeRules: string[] = [];
+
+    private stringExcludeRules: string[] = [];
+    private stringIncludeRules: string[] = [];
+
+    constructor(config: IClassStringConfiguration) {
+        this.config = config;
+
+        // Build up the internal reference lists.
+        this.directiveExcludeRules = this.getExcludeRules(config.directives);
+        this.directiveIncludeRules = this.getIncludeRules(config.directives);
+
+        this.phpTagExcludeRules = this.getExcludeRules(config.phpTagRules);
+        this.phpTagIncludeRules = this.getIncludeRules(config.phpTagRules);
+
+        this.echoExcludeRules = this.getExcludeRules(config.bladeEchoRules);
+        this.echoIncludeRules = this.getIncludeRules(config.bladeEchoRules);
+
+        this.documentExcludeRules = this.getExcludeRules(config.documentRules);
+        this.documentIncludeRules = this.getIncludeRules(config.documentRules);
+
+        this.stringExcludeRules = this.getExcludeRules(config.stringRules);
+        this.stringIncludeRules = this.getIncludeRules(config.stringRules);
+    }
+
+    private getIncludeRules(rules: IClassRuleset[]): string[] {
+        let includeRules: string[] = [];
+
+        rules.forEach((rule) => {
+            includeRules = includeRules.concat(rule.includeWhen);
+        });
+
+        return includeRules;
+    }
+
+    private getExcludeRules(rules: IClassRuleset[]): string[] {
+        let excludeRules: string[] = [];
+
+        rules.forEach((rule) => {
+            excludeRules = excludeRules.concat(rule.excludeWhen);
+        });
+
+        return excludeRules;
+    }
+
+    private passes(content: string, patterns: string[]): boolean {
+        if (patterns.length == 0) {
+            return true;
+        }
+
+        for (let i = 0; i < patterns.length; i++) {
+            if ((new RegExp(patterns[i])).test(content)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    canTransformDocument(document: BladeDocument): boolean {
+        if (!this.config.enabled) {
+            return false;
+        }
+
+        const docText = document.getContent();
+
+        if (!this.passes(docText, this.documentExcludeRules)) {
+            return false;
+        }
+
+        return this.passes(docText, this.documentIncludeRules);
+    }
+
+    private canTransform(content: string, specificExcludes: string[], specificIncludes: string[]): boolean {
+        if (this.passes(content, specificExcludes)) {
+            return false;
+        }
+
+        if (!this.passes(content, specificIncludes)) {
+            return false;
+        }
+
+        // Test against the generic rules, if available.
+        if (this.passes(content, this.stringExcludeRules)) {
+            return false;
+        }
+
+        return this.passes(content, this.stringIncludeRules);
+    }
+
+    canTransformBladeEcho(echo: BladeEchoNode): boolean {
+        if (!this.config.enabled || !this.config.bladeEchoEnabled) {
+            return false;
+        }
+
+        return this.canTransform(echo.content, this.echoExcludeRules, this.echoIncludeRules);
+    }
+
+    canTransformBladePhp(bladePhp: DirectiveNode): boolean {
+        if (!this.config.enabled || !this.config.bladePhpEnabled) {
+            return false;
+        }
+
+        return this.canTransform(bladePhp.documentContent, this.phpTagExcludeRules, this.phpTagIncludeRules);
+    }
+
+    canTransformInlinePhp(inlinePhp: InlinePhpNode): boolean {
+        if (!this.config.enabled || !this.config.phpTagsEnabled) {
+            return false;
+        }
+
+        return this.canTransform(inlinePhp.sourceContent, this.phpTagExcludeRules, this.phpTagIncludeRules);
+    }
+
+    canTransformDirective(directiveNode: DirectiveNode): boolean {
+        if (!this.config.enabled || !this.config.directivesEnabled) {
+            return false;
+        }
+
+        let checkName = directiveNode.directiveName;
+
+        if (directiveNode.originalAbstractNode != null) {
+            if (directiveNode.originalAbstractNode instanceof DirectiveNode) {
+                checkName = directiveNode.originalAbstractNode.directiveName;
+            }
+        }
+
+        checkName = checkName.toLowerCase();
+
+        if (this.config.excludedDirectives.length > 0 && this.config.excludedDirectives.includes(checkName)) {
+            return false;
+        }
+
+        return this.canTransform(directiveNode.sourceContent, this.directiveExcludeRules, this.directiveIncludeRules);
+    }
+}
