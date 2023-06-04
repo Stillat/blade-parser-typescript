52c56036c58c71048e54c2813ddb8bb646a3feb5
John Koster <john@stillat.com>
2023-06-04 16:53:05 -0500

    Update parser.ts

--- src/parser.ts
+++ src/parser.ts
@@ -1,19 +1,3 @@
 // Internal file used to debug the parser.
 
-import { PhpStructuresAnalyzer } from './analyzers/phpStructuresAnalyzer';
-import { BladeDocument } from './document/bladeDocument';
-import { ClassStringEmulation } from './formatting/classStringEmulation';
-import { formatBladeString, formatBladeStringWithPint } from './formatting/prettier/utils';
-import { ClassEmulator } from './parser/classEmulator';
-import { InlineStringParser } from './parser/inlineStringParser';
-const espree = require('espree');
-const code = `
-
-
-@php
-    $shouldIgnore = "one \${three}";
-@endphp
-
-`;
-console.log(formatBladeString(code));
 debugger;
